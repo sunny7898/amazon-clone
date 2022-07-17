d034f2fcd261d625d8df172677100fd67d1277dc
Sunny Goswami <sungoswami07@gmail.com>
2022-07-17 17:22:00 +0530

    header intial layout done

---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="ahttps://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <img src="search_icon" />
+      </div>
+
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
