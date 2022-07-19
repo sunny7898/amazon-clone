3f8eef3681f43ed8135f24ceb16276e5ef1c659e
Sunny Goswami <sungoswami07@gmail.com>
2022-07-19 08:10:02 +0530

    Checkout Start

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SharedLayout from "./SharedLayout";
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SharedLayout />}>
             <Route index element={<Home />} />
-            <Route path="checkout" element={<h1>This is checkout page!</h1>} />
+            <Route path="checkout" element={<Checkout />} />
           </Route>
         </Routes>
       </div>
